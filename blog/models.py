7ba7d044d0fa54411bdd74eef049e958914a7f33
yoyongbo <yoyu@upei.ca>
2023-06-28 15:28:17 -0400

    add files

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    """
+    Each Post will be a new table in the db 
+    """
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # optional
+    # pic = models.ImageField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
+    # This is same as java toString method, the job is print friendly text to represent the object
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here. Main page to edit the home page
 
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
 
 def home(request):
     # The post context
     context = {
-        ''
-        'posts': posts
+        # todo: add title
+        'title': 'Home',
+        'posts': Post.objects.all()
     }
+
     return render(request, 'blog/home.html', context)
 
 def about(request):
